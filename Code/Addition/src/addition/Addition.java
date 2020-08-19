5f85853123f583091973e89a7bbc5d70e85558f5
Manohar Vemuri <29159385+mvemuri6642@users.noreply.github.com>
2020-08-19 16:08:33 +0530

    core_programs

---
+++ Code/Addition/src/addition/Addition.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addition;
+
+/**
+ *
+ * @author Manohar V
+ */
+public class Addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10;
+        int b=15;
+        int c=a+b;
+        System.out.println(c);
+    }
+    
+}
---
+++ Code/ArithmeticOperations/src/arithmeticoperations/ArithmeticOperations.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmeticoperations;
+
+/**
+ *
+ * @author Manohar V
+ */
+public class ArithmeticOperations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10;
+        int b=5;
+        System.out.println("Addition:"+(a+b)+"\nSubtraction:"+(a-b)+"\nMultiplication:"+(a*b)+"\nDivision:"+(a/b));
+    }
+    
+}
