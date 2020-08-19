a91a2039eff08c71a7abe13243bae6ad516cdbb5
Manohar Vemuri <29159385+mvemuri6642@users.noreply.github.com>
2020-08-19 15:54:12 +0530

    new

--- Code/Addition/src/addition/Addition.java
+++ Code/Addition/src/addition/Addition.java
@@ -16,6 +16,11 @@ public class Addition {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int lightspeed=299792;
+        long days=1000;
+        long seconds=days*24*60*60;
+        long distance=lightspeed*seconds;
+        System.out.println("In "+days+ " days light will travel about "+distance+ " kilometers");
     }
     
 }
