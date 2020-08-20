b499aca9a69ec4d2c3856091c922e72b42e58dbc
Manohar Vemuri <29159385+mvemuri6642@users.noreply.github.com>
2020-08-20 19:22:42 +0530

    new

---
+++ Datatypes_Examples/src/datatypes_examples/Datatypes_Examples.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatypes_examples;
+
+/**
+ *
+ * @author Manohar V
+ */
+public class Datatypes_Examples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //char datatype
+        /**
+        char ch1,ch2;
+        ch1=88; //ASCII code for x
+        ch2='Y';
+        System.out.println(ch1+" "+ch2);
+        //increment the value of character variable
+        ch1++;
+        System.out.println(ch1);
+        * */
+        boolean b;
+        b=true;
+        System.out.println("b is "+b);
+        if(b) System.out.println("This is printed if b is "+b);
+        b=false;
+        System.out.println("b is "+b);
+        System.out.println("3>2 is "+(3>2));
+    }
+    
+}
---
+++ circleArea/src/circlearea/CircleArea.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circlearea;
+
+/**
+ *
+ * @author Manohar V
+ */
+public class CircleArea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double pi,r,a;
+        r=5;
+        pi=3.1416;
+        a=pi*r*r;
+        System.out.println(a);
+    }
+    
+}
